f0d99de2be0f771e9461c030afae6bbeeba7e3a5
efossas <serpicofossas@gmail.com>
2018-02-05 22:15:36 -0700

    travis permissions

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -14,13 +14,6 @@ BLOCKS=$(ls -l "$SCRIPTPATH"/../blocks | tail -n +2 | tr -s ' ' | cut -d" " -f9)
 for name in $BLOCKS; do ln -sf "../../blocks/$name/$name.js" "${SCRIPTPATH}/../public/blocks/$name.js"; done
 unset BLOCKS
 
-# get config file
-if [ $# == 1 ]; then
-    CONFIG="$SCRIPTPATH/../$1"
-else
-    CONFIG="$SCRIPTPATH/../config.json"
-fi
-
 # generate ssl certificates
 DOMAIN=$(cat $SCRIPTPATH/../config.json | grep "domain" | cut -d'"' -f4 | cut -d'/' -f3)
 "$SCRIPTPATH"/../tools/gencert.sh "$DOMAIN"
