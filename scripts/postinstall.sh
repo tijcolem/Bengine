4332a65b3e245f3e99841adbf353b4a80117d24a
efossas <serpicofossas@gmail.com>
2018-02-05 21:51:13 -0700

    reorganized scripts for testing

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+  
+SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+SCRIPTPATH=`dirname $SCRIPT`
+
 # create symlinks to
 ln -sf ../../node_modules/mocha/mocha.css ./public/css
 ln -sf ../../node_modules/mocha/mocha.js ./public/js
@@ -8,3 +13,17 @@ rm -rf public/blocks/*
 BLOCKS=$(ls -l ./blocks | tail -n +2 | tr -s ' ' | cut -d" " -f9)
 for name in $BLOCKS; do ln -s "../../blocks/$name/$name.js" "./public/blocks/$name.js"; done
 unset BLOCKS
+
+# get config file
+if [ $# == 1 ]; then
+    CONFIG="$SCRIPTPATH/../$1"
+else
+    CONFIG="$SCRIPTPATH/../config.json"
+fi
+
+# generate ssl certificates
+DOMAIN=$(cat $SCRIPTPATH/../config.json | grep "domain" | cut -d'"' -f4 | cut -d'/' -f3)
+"$SCRIPTPATH"/../tools/gencert.sh "$DOMAIN"
+
+cd $SCRIPTPATH;
+
