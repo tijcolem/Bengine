38ce0b2f83a42abb54de50c8479c93a947408104
efossas <serpicofossas@gmail.com>
2018-02-13 18:45:15 -0700

    updated gencert paths

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,6 +3,8 @@
 SCRIPT="$0"
 SCRIPTPATH=$(dirname "$SCRIPT")  
 
+echo "scriptpath: $SCRIPTPATH"
+
 # create symlinks to
 ln -sf ../../node_modules/mocha/mocha.css ${SCRIPTPATH}/../public/css
 ln -sf ../../node_modules/mocha/mocha.js ${SCRIPTPATH}/../public/js
@@ -15,8 +17,11 @@ for name in $BLOCKS; do ln -sf "../../blocks/$name/$name.js" "${SCRIPTPATH}/../p
 unset BLOCKS
 
 # generate ssl certificates
-DOMAIN=$(cat $SCRIPTPATH/../config.json | grep "domain" | cut -d'"' -f4 | cut -d'/' -f3)
+DOMAIN=$(cat $SCRIPTPATH/../config/config.json | grep "domain" | cut -d'"' -f4 | cut -d'/' -f3)
 "$SCRIPTPATH"/../tools/gencert.sh "$DOMAIN"
+
+rm -rf ../ssl
+mv ssl ../ssl
 
 cd $SCRIPTPATH;
 
